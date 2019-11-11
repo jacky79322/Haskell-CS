1ed9d2a90d26b959bc16831a8c2a04d309f4bb86
jacky79322 <48339258+jacky79322@users.noreply.github.com>
2019-11-11 02:06:53 -0800

    Added doubleSmallNumber

--- baby.hs
+++ baby.hs
@@ -1,3 +1,8 @@
 doubleMe x = x + x
 
 doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100  
+                        then x  
+                        else x*2 --else statements are required with if statements
+                        -- optional: doubleSmallNumber' x = (if x > 100 then x else x*2) + 1  
