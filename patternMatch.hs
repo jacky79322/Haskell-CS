7a44cecd60924506f8ffe1f744c29bbec4318b8e
jacky79322 <48339258+jacky79322@users.noreply.github.com>
2019-11-11 10:52:55 -0800

    updated functions

--- patternMatch.hs
+++ patternMatch.hs
@@ -20,4 +20,41 @@ charName 'a' = "Albert"
 charName 'b' = "Broseph"  
 charName 'c' = "Cecil" 
 charName x = "Can't find listed name, sorry :p"
-//prevents an exception by including a catch-all pattern
+--prevents an exception by including a catch-all pattern
+
+
+--without pattern matching
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)  
+addVectors a b = (fst a + fst b, snd a + snd b) 
+
+--with pattern matching
+addVectors :: (Num a) => (a, a) -> (a, a) -> (a, a)  
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)  
+
+--3 components of triplets
+first :: (a, b, c) -> a  
+first (x, _, _) = x  
+  
+second :: (a, b, c) -> b  
+second (_, y, _) = y  
+  
+third :: (a, b, c) -> c  
+third (_, _, z) = z  
+
+--head function
+head' :: [a] -> a  
+head' [] = error "Can't call head on an empty list, dummy!"  
+head' (x:_) = x  
+
+--list elements
+tell :: (Show a) => [a] -> String  
+tell [] = "The list is empty"  
+tell (x:[]) = "The list has one element: " ++ show x  
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y  
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y  
+
+--length
+length' :: (Num b) => [a] -> b  
+length' [] = 0  
+length' (_:xs) = 1 + length' xs  
+
