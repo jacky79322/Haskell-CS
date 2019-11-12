617922616862577f5ce39cfa19c3e6a6a5a9f277
jacky79322 <48339258+jacky79322@users.noreply.github.com>
2019-11-11 18:58:21 -0800

    Add files via upload

---
+++ modules.hs
@@ -0,0 +1,6 @@
+--Data.List , Data.Map, Data.Set functionalities
+
+--use import to use these sets of functions
+--import qualified to specify for our functions
+
+--making our own modules with geometry (included in a subfile
