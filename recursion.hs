d5d6d78ca61e92e70184ec167185a71d813c1fa8
jacky79322 <48339258+jacky79322@users.noreply.github.com>
2019-11-11 16:42:25 -0800

    Add files via upload

---
+++ recursion.hs
@@ -0,0 +1,58 @@
+
+--maximum 
+maximum' :: (Ord a) => [a] -> a  
+maximum' [] = error "maximum of empty list"  
+maximum' [x] = x  
+maximum' (x:xs)   
+    | x > maxTail = x  
+    | otherwise = maxTail  
+    where maxTail = maximum' xs  
+--maximum2	
+maximum'2 :: (Ord a) => [a] -> a  
+maximum'2 [] = error "maximum of empty list"  
+maximum'2 [x] = x  
+maximum'2 (x:xs) = max x (maximum' xs)  
+
+--replicate
+replicate' :: (Num i, Ord i) => i -> a -> [a]  
+replicate' n x  
+    | n <= 0    = []  
+    | otherwise = x:replicate' (n-1) x  
+
+--take
+take' :: (Num i, Ord i) => i -> [a] -> [a]  
+take' n _  
+    | n <= 0   = []  
+take' _ []     = []  
+take' n (x:xs) = x : take' (n-1) xs  
+
+--reverse
+reverse' :: [a] -> [a]  
+reverse' [] = []  
+reverse' (x:xs) = reverse' xs ++ [x]  
+
+--repeat
+repeat' :: a -> [a]  
+repeat' x = x:repeat' x 
+
+--zip
+zip' :: [a] -> [b] -> [(a,b)]  
+zip' _ [] = []  
+zip' [] _ = []  
+zip' (x:xs) (y:ys) = (x,y):zip' xs ys  
+
+--elem
+elem' :: (Eq a) => a -> [a] -> Bool  
+elem' a [] = False  
+elem' a (x:xs)  
+    | a == x    = True  
+    | otherwise = a `elem'` xs   
+	
+--quicksort
+quicksort :: (Ord a) => [a] -> [a]  
+quicksort [] = []  
+quicksort (x:xs) =   
+    let smallerSorted = quicksort [a | a <- xs, a <= x]  
+        biggerSorted = quicksort [a | a <- xs, a > x]  
+    in  smallerSorted ++ [x] ++ biggerSorted  
+
